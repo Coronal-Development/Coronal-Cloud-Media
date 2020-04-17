4175b33515613a21ed53a29988dc34607e99d9a2
Media <Media@DESKTOP-DNM93VI>
2020-04-17 15:24:27 +0800

    Addction directory guide file an common file. 	new file:   docs/Directory Guide.md 	new file:   src/common/common.h 	new file:   src/common/singleton.h

---
+++ src/common/common.h
@@ -0,0 +1,21 @@
+#ifndef CCM_LOG_COMMON_H_
+#define CCM_LOG_COMMON_H_
+
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <tuple>
+#include <fstream>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <thread>
+#include <locale>
+#include <sstream>
+#include <mutex>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <list>
+
+#endif // !CCM_LOG_COMMON_H_
